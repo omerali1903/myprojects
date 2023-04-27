e482c65904e3f508bdb6b91e1b1bd3e12116c0ba
omerali1903 <131624938+omerali1903@users.noreply.github.com>
2023-04-27 13:44:53 -0500

    Add files via upload

---
+++ MyGitHubProjectNT/src/AtmProject.java
@@ -0,0 +1,16 @@
+public class AtmProject {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bu bir ATM Projesidir");
+        //version 1
+        System.out.println("Java is easy");
+        //version 2
+        System.out.println("Learn Java Earn Money");
+        //version 3
+        System.out.println("Git ogrenmek cok kolay");
+        //Version 4g
+
+    }
+
+}
