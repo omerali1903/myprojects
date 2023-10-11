f9abe67741c91f4e68b40ed027e9650781ee99c7
omerali1903 <131624938+omerali1903@users.noreply.github.com>
2023-10-11 10:54:24 -0500

    Update AtmProject.java

--- MyGitHubProjectNT/src/AtmProject.java
+++ MyGitHubProjectNT/src/AtmProject.java
@@ -2,13 +2,13 @@ public class AtmProject {
 
     public static void main(String[] args) {
 
-        System.out.println("Bu bir ATM Projesidir");
+        System.out.println("This is a Atm Project");
         //version 1
         System.out.println("Java is easy");
         //version 2
-        System.out.println("Learn Java Earn Money");
+        System.out.println("Learn Java for future.");
         //version 3
-        System.out.println("Git ogrenmek cok kolay");
+        System.out.println("I learning git");
         //Version 4g
 
     }
