07f4bd0acd0fc151765a7164b3d65d6b838827ac
omerali1903 <131624938+omerali1903@users.noreply.github.com>
2023-10-11 10:53:31 -0500

    Update Account.java

--- MyGitHubProjectNT/src/Account.java
+++ MyGitHubProjectNT/src/Account.java
@@ -1,6 +1,6 @@
 public class Account {
     public static void main(String[] args) {
-        System.out.println("bazi kodlar yazdim..");
+        System.out.println("i wrote some codes here..");
 
     }
 
